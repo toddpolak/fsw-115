e824a6fd1f8e9af99cde066afcdd70bc55873b2e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-28 17:11:57 -0700

    Postman Exercise

---
+++ week2/postman-exercise/index.js
@@ -0,0 +1,15 @@
+
+const json = '';
+//console.log(JSON.parse(json));
+//let content = document.getElementById('content');
+//content.innerHTML = JSON.parse(json);
+
+
+for (let i = 0; i < json.length; i++) {
+
+    console.log(json[i])
+
+    //let div = document.createElement('div');
+    //div.innerHTML = 'Title: ' + json[i];
+    //content.appendChild(div);
+}
