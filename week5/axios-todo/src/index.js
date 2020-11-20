c474165ae3dd8753f24afe2d1a54de69c61a4c39
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-11-20 12:27:51 -0800

    Axios Todo

--- week5/axios-todo/src/index.js
+++ week5/axios-todo/src/index.js
@@ -1,38 +1,89 @@
 const todoForm = document.todoForm
 const content = document.getElementById('content')
-const ol = document.createElement('ol')
 
 axios.get('https://api.vschool.io/toddpolak/todo/')
     .then(response => {
-
-        for (let i = 0; i < response.data.length; i++) {
-
-            let li = document.createElement('li')
-            let title = document.createElement('h3')
-            let description = document.createElement('h4')
-            let img = document.createElement('img')
-
-            title.textContent = response.data[i].title
-            description.textContent = response.data[i].description
-
-            if (response.data[i].imgUrl !== '') {
-                img.src = response.data[i].imgUrl
-                img.style.width = "100px"
-                img.style.height = "100px"
-            }
-
-            if (response.data[i].completed) {
-                title.style.textDecorationLine = 'line-through'
-            }
-
-            li.appendChild(title)
-            li.appendChild(description)
-            li.appendChild(img)
-            ol.appendChild(li)
-        }
-        content.appendChild(ol)
+        showTodos(response)
     })
     .catch(error => console.log(error))
+
+function clearTodos() {
+    while(content.firstChild){
+        content.removeChild(content.firstChild)
+    }
+}
+
+
+
+function showTodos(response) {
+
+    for (let i = 0; i < response.data.length; i++) {
+
+        let ol = document.createElement('ol')
+        let li = document.createElement('li')
+        let title = document.createElement('h3')
+        let description = document.createElement('h4')
+        let img = document.createElement('img')
+
+        let mainDiv = document.createElement('div')
+        let leftDiv = document.createElement('div')
+        let rightDiv = document.createElement('div')
+
+        let checkbox = document.createElement('input')
+        checkbox.type = 'checkbox'
+        checkbox.name = 'completed'
+        checkbox.checked = response.data[i].completed
+
+        checkbox.onclick = function() {
+            alert('click')
+        }
+
+        mainDiv.style.border = "3px dashed black"
+        mainDiv.style.width = "600px"
+
+        leftDiv.style.border = "1px solid red"
+        leftDiv.style.display = "inline-block"
+        leftDiv.style.margin = "0px"
+        leftDiv.style.width = "400px"
+
+        rightDiv.style.border = "3px solid green"
+        rightDiv.style.display = "inline-block"
+        rightDiv.style.margin = "0px"
+        rightDiv.style.width = "100px"
+
+        title.textContent = response.data[i].title
+
+        description.textContent = response.data[i].description
+
+        console.log(response.data[i].title, 'image: ' + response.data[i].imgUrl)
+
+        if (response.data[i].imgUrl !== undefined) {
+            img.src = response.data[i].imgUrl
+            img.style.width = "70px"
+            img.style.height = "70px"
+        }
+
+        if (response.data[i].completed) {
+            title.style.textDecorationLine = 'line-through'
+        }
+        li.appendChild(title)
+
+        if (response.data[i].description !== undefined) {
+            li.appendChild(description)
+        }
+
+        li.appendChild(checkbox)
+
+        ol.appendChild(li)
+
+        leftDiv.appendChild(ol)
+        rightDiv.appendChild(img)
+        mainDiv.appendChild(leftDiv)
+        mainDiv.appendChild(rightDiv)
+
+        content.appendChild(mainDiv)
+    }
+}
 
 todoForm.addEventListener("submit", function(event) {
     event.preventDefault()
@@ -45,6 +96,8 @@ todoForm.addEventListener("submit", function(event) {
     }
 
     axios.post('https://api.vschool.io/toddpolak/todo/', newTodo)
-        .then(response => console.log(response.data))
+        .then(response => {
+        showTodos(response)
+    })
         .catch(error => console.log(error))
 })
