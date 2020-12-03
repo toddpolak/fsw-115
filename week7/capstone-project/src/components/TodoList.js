42af94c49d3b32027a5cab342dfe1b30225a1e33
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-12-02 18:30:16 -0800

    Capstone Project

--- week7/capstone-project/src/components/TodoList.js
+++ week7/capstone-project/src/components/TodoList.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-//import Todos from '../components/Todos'
 
 class TodoEntry extends React.Component {
     constructor() {
@@ -11,31 +10,29 @@ class TodoEntry extends React.Component {
             id: '',
             title: ''
         }
-
         this.editButtonClickHandler = this.editButtonClickHandler.bind(this)
         this.titleChangeHandler = this.titleChangeHandler.bind(this)
         this.saveButtonClickHandler = this.saveButtonClickHandler.bind(this)
     }
 
     componentDidMount() {
+
         axios.get('https://api.vschool.io/toddpolak/todo/')
             .then(response => {
-                const todos = response.data
+                let todos = response.data
                 this.setState({todos})
             })
     }
 
     editRenderer(todo, fieldName) {
         if (this.state.id && this.state.id === todo._id) {
-            const saveButtonId = `save-${todo._id}`
-            const cancelButtonId = `cancel-${todo._id}`
             return (
                 <div>
                     <button id={todo._id}
                         onClick={this.saveButtonClickHandler}>
                         Save
                     </button>
-                    <button id={cancelButtonId}
+                    <button 
                         onClick={() => console.log('CANCEL CLICKED')}>
                         Cancel
                     </button>
@@ -57,21 +54,34 @@ class TodoEntry extends React.Component {
     }
 
     titleChangeHandler(event) {
-        console.log(event.target.value)
         this.setState({title: event.target.value})
     }
 
     saveButtonClickHandler(event) {
 
-        console.log('event.target.id: ', event.target.id)
+        const updates = {
+            title: this.state.title
+        }
 
-        axios.put('https://api.vschool.io/toddpolak/todo/' + event.target.id, {
-                'title': this.state.title
-        })
+        axios.put('https://api.vschool.io/toddpolak/todo/' + event.target.id, updates)
+
+        
+
+            
+            .then(async response => {
+                const response_1 = await axios.get('https://api.vschool.io/toddpolak/todo/')
+                let todos = response_1.data
+                this.setState({ todos })
+            })
+            
+
+        this.setState({ id: ''})
+
 
     }
 
-    titleRenderer(todo, fieldName) {
+    titleRenderer(todo) {
+
         if (this.state.id && this.state.id === todo._id) {
             return (
                 <div>
@@ -89,12 +99,10 @@ class TodoEntry extends React.Component {
 
     render() {
 
-        console.log('this.state.todos: ', this.state.todos)
-
         return (
             <ul>
                 {this.state.todos.map((todo, index) => 
-                
+
                 <li key={index}>
 
                     {this.titleRenderer(todo, 'title')}
@@ -106,85 +114,6 @@ class TodoEntry extends React.Component {
             </ul>
         )
     }
-
-    /*
-    handleChange(event) {
-        const {name, value} = event.target
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleClick(event) {
-        event.preventDefault()
-
-        let todoEntry = {
-            title: this.state.title,
-            description: this.state.description
-        }
-
-        this.setState({
-            todos: [...this.state.todos, todoEntry]
-        })
-
-        this.setState({
-            title: '',
-            description: ''
-        })
-
-    }
-
-    completed() {
-        if (this.state.title.length >=3 
-            && this.state.description.length >=3) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    render() {
-        return (
-            <div style={{display: "flex", justifyContent: "center"}}>
-                <div>
-                    <div style={{width: "600px", 
-                        border: "1px solid black",
-                        borderRadius: "5px",
-                        padding: "10px"}}>
-                        <h3>Todo Entry</h3>
-                        <form>
-                        <div style={{textAlign: "center", padding: "10px"}}>
-                            <input 
-                                type="text" 
-                                className="right_column" 
-                                value={this.state.title} 
-                                name="title" 
-                                placeholder="Title" 
-                                onChange={this.handleChange} />
-                        </div>
-                        <div style={{textAlign: "center"}}>
-                            <textarea 
-                                style={{resize: "none"}}
-                                rows={6} 
-                                cols={56} 
-                                value={this.state.description} 
-                                name="description" 
-                                placeholder="Description" 
-                                onChange={this.handleChange} />
-                        </div>
-                        <div style={{textAlign: "center", padding: "10px"}}>
-                            <button 
-                                disabled={!this.completed()} 
-                                onClick={this.handleClick}>Add Todo</button>
-                        </div>
-                    </form>
-                    </div>
-                    <div><Todos todos={this.state.todos} /></div>
-                </div>
-            </div>
-        )
-    }
-    */
 }
 
 export default TodoEntry
